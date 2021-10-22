0ebf2b65d9a83a8fcc05d8b78995275c9ba755ff
Bhumika1524 <51358927+Bhumika1524@users.noreply.github.com>
2021-10-22 17:45:11 +0530

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :writers
-  root to: "writers#index"
+  resources :users
+  root to: 'users#index'
+  # get '/new', to: 'users#new'
+  # post '/new', to: 'users#create'
+  # get '/edit', to: 'users#edit'
+  # post '/edit', to: 'users#update'
+  # patch '/edit', to: 'users#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
